092c08461131eac081c0d28ecaeb692967bb8d91
MariusFogelgren <marius.fogelgren@lego.com>
2024-09-20 14:25:17 +0200

    First part code

--- Todo List/Program.cs
+++ Todo List/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Todo_List
+﻿using System.Runtime.InteropServices;
+
+namespace Todo_List
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a todo list");
+            Console.WriteLine("\nHere are your choices");
+            Console.WriteLine("1. Add new tasks");
+            Console.WriteLine("2. Print all tasks");
+            Console.WriteLine("3. delete tasks");
+            Console.WriteLine("4. Mark a task done");
+            Console.Write("\nEnter your choice: ");
+            var todoListInput = Console.ReadLine();
+
+
+            switch (todoListInput)
+            {
+                case "1":
+                    Console.WriteLine();
+                    break;
+                case "2":
+                    Console.WriteLine();
+                    break;
+                case "3":
+                    Console.WriteLine();
+                    break;
+                case "4":
+                    Console.WriteLine();
+                    break;
+
+            };
+
+            
+
+
+        }
+        class TodoListCase 
+        {
+            
         }
     }
 }
