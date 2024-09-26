56afd75fa6a0db2273bfa7a68655bfd9801b79a3
MariusFogelgren <marius.fogelgren@lego.com>
2024-09-26 14:55:40 +0200

    Page switch maybe work.

--- WpfApp1/Views/MainWindow.xaml.cs
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -1,57 +1,18 @@
-﻿using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WpfApp1
+namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        private readonly NavigationService _navigationService;
-        public static NavigationService NavigationService => ((MainWindow)Application.Current.MainWindow).frmMain.NavigationService;
-
-
-        //public MainWindow(NavigationService navigationService)
         public MainWindow()
         {
             InitializeComponent();
-            _navigationService = NavigationService.GetNavigationService(this);
         }
 
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NavigateButton_Click(object sender, RoutedEventArgs e)
         {
-
-            _navigationService.Navigate(new Uri("Page1.xaml", UriKind.Relative));
-
+            // Navigate to Page1
+            MainFrame.Navigate(new Page1());
         }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-
-
     }
 }
