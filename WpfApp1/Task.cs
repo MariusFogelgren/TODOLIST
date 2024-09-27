0568f9485d399f5e2f48d2104b41ee45f6bb8b70
MariusFogelgren <marius.fogelgren@lego.com>
2024-09-27 11:02:15 +0200

    Addtask_Click added to code

---
+++ WpfApp1/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Task
+    {
+        public string Title { get; set; }
+        public string Status { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- WpfApp1/Views/MainWindow.xaml.cs
+++ WpfApp1/Views/MainWindow.xaml.cs
@@ -1,13 +1,55 @@
 ﻿using System.Windows;
+using System.Windows.Markup;
+using WpfApp1;
+using Task = WpfApp1.Task;
+
 
 namespace WpfApp
 {
     public partial class MainWindow : Window
     {
+        private List<Task> taskList = new List<Task>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        
+
+        private void addTask_Click (object sender, RoutedEventArgs e)
+        {
+            var test = textBoxTask.Text;
+
+            var newTask = new WpfApp1.Task() 
+            { 
+                Title = test, 
+                Status = "not done",
+                Date = DateTime.Now,
+            };
+
+            taskList.Add(newTask);
+        }
+
+        private void printTask (object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void deleteTask (object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void markTask(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+        }
+
+       
     }
 }
